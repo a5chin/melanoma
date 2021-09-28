f6b93467467b20e673db89ea60f1976c0a57c64a
a5chin <81167661+a5chin@users.noreply.github.com>
2021-09-28 12:22:17 +0900

    fixed bugs (#1)
    Co-authored-by: Eigo Hara <hara.e@MD.LOCAL>

--- lib/config/default.py
+++ lib/config/default.py
@@ -1,16 +1,15 @@
 from yacs.config import CfgNode as CN
-from pathlib import Path
 
 _C = CN()
 
 _C.DATASET = CN()
 _C.DATASET.NAME = 'SIIM-ISIC Melanoma'
-_C.DATASET.ROOT = 'path to root'
+_C.DATASET.ROOT = r'\\aka\data\medical\siim-isic-melanoma-classification'
 _C.DATASET.NUM_CLASSES = 2
-_C.DATASET.IMAGE_TRAIN = _C.DATASET.ROOT / Path('jpeg/train')
-_C.DATASET.IMAGE_VAL = _C.DATASET.ROOT / Path('jpeg/test')
-_C.DATASET.IDS_TRAIN = _C.DATASET.ROOT / Path('train.csv')
-_C.DATASET.IDS_VAL = _C.DATASET.ROOT / Path('test.csv')
+_C.DATASET.IMAGE_TRAIN = _C.DATASET.ROOT + '/jpeg/train'
+_C.DATASET.IMAGE_VAL = _C.DATASET.ROOT + '/jpeg/test'
+_C.DATASET.IDS_TRAIN = _C.DATASET.ROOT + '/train.csv'
+_C.DATASET.IDS_VAL = _C.DATASET.ROOT + '/test.csv'
 _C.DATASET.NUM_CLASSES_AGE = 17
 _C.DATASET.NUM_CLASSES_SITE = 6
 _C.DATASET.BATCH_SIZE = 128
@@ -18,8 +17,8 @@ _C.DATASET.TOTAL_EPOCH = 20
 
 _C.MODEL = CN()
 _C.MODEL.NAME = ''
-_C.MODEL.LOG_DIR = Path('./logs')
-_C.MODEL.SAVED_DIR = Path('./saved_models')
+_C.MODEL.LOG_DIR = './logs'
+_C.MODEL.SAVED_DIR = './saved_models'
 _C.MODEL.PRETRAINED = ''
 _C.MODEL.OPTIMIZER = ''
 _C.MODEL.CRITERION = ''
